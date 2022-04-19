045039ce89cc5bc25f95b0415c95ef5f0ce33850
Karim <nogipx@gmail.com>
2022-04-19 17:50:32 +0300

    Revert "add sentry"
    This reverts commit af631ebfb4f3bc373dd532b2d5ba01739cc60254.

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:simpledo/di.dart';
 import 'package:simpledo/screens/main_screen.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
   unawaited(SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
-  ]));
-
+  ]);
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
     // iOS
     statusBarColor: Colors.white,
@@ -26,18 +22,14 @@ Future<void> main() async {
     systemNavigationBarIconBrightness: Brightness.dark,
   ));
 
-  final app = Injector(
-    child: const MainScreen(),
-  );
-  await app.init();
-
-  await SentryFlutter.init(
-    (options) {
-      options.dsn =
-          'https://9360e7b51272491188937f9fd92bed0e@o358023.ingest.sentry.io/6346375';
-    },
-    appRunner: () => runApp(
-      MaterialApp(
+  WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded(
+    () async {
+      final app = Injector(
+        child: const MainScreen(),
+      );
+      await app.init();
+      runApp(MaterialApp(
         home: ConstrainedBox(
           constraints: const BoxConstraints(
             maxWidth: 600,
@@ -48,7 +40,8 @@ Future<void> main() async {
         theme: ThemeData(
           colorScheme: const ColorScheme.light(),
         ),
-      ),
-    ),
+      ));
+    },
+    (error, stackTrace) {},
   );
 }
